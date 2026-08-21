ae9b21f4262fefbc31cd5546b8bcb771dd205ed0
Ana Andrade <andradeanavida@gmail.com>
2026-08-20 21:39:21 -0300

    s

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Olá, Flask está funcionando!'
+    return render_template('index.html')
 
 @app.route('/sobre')
 def sobre():
