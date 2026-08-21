42cc19cc87f96b81254edd79bfbaed012e6c4403
Ana Andrade <andradeanavida@gmail.com>
2026-08-20 21:33:32 -0300

    teres

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return 'Olá, Flask está funcionando!'
+
+@app.route('/sobre')
+def sobre():
+    return 'Sobre o sistema'
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
